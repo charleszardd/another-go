fd166dc584894d7f48411d954430d7fdb12de5aa
Peerspective <112449746+Peerspective@users.noreply.github.com>
2025-05-26 17:59:54 +0800

    added unit testing

--- greetings/greetings_test.go
+++ greetings/greetings_test.go
@@ -9,15 +9,15 @@ import(
 func TestHelloName(t *testing.T) {
 	name := "Charles"
 	want := regexp.MustCompile(`/b`+name+`/b`)
-	msg, _ ,_ error := Greet("Charles", 12)
+	msg, error, _,_ := Greet("Charles", 12)
 	  if !want.MatchString(msg) || error != nil {
         t.Errorf(`Hello("Gladys") = %q, %v, want match for %#q, nil`, msg, error, want)
     }
 }
 
 func TestHelloEmpty(t *testing.T) {
-    msg,_,_ error := Hello("", 12)
-    if msg != "" || err == nil {
+    msg,error,_,_ := Greet("", 12)
+    if msg != "" || error == nil {
         t.Errorf(`Hello("") = %q, %v, want "", error`, msg, error)
     }
 }
